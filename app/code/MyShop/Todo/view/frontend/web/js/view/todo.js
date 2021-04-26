ed289d27969c224f6a89bf6621b176ac4d683ddc
jamiul <jamiulewu24@gmail.com>
2021-04-26 15:51:31 +0600

    ui component via jsLayout

--- app/code/MyShop/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MyShop/Todo/view/frontend/web/js/view/todo.js
@@ -1,5 +1,15 @@
 define(['uiComponent'], function (Component) {
     'use strict';
     console.log("click");
-    return Component.extend({})
+    return Component.extend({
+        defaults: {
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"},
+                {label: "Task 5"},
+            ]
+        }
+    });
 });
